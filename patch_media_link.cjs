18f116f78ede638ddeeeabeb762cc0438d7426bd
Yashwanth <yashwanth.m1ga16me123@gmail.com>
2026-07-13 15:12:13 +0530

    Clearing files to shift framework to no-build approach

--- patch_media_link.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-let css = fs.readFileSync('src/index.css', 'utf8');
-css = css.replace(
-  /flex: 1 1 auto !important; \/\* Take up majority of the space \*\/\n\s*height: 0 !important; \/\* Allow flex to size it \*\/\n\s*display: flex !important;/g,
-  'flex: none !important;\n  height: auto !important;\n  aspect-ratio: 16 / 10 !important;\n  display: block !important;'
-);
-fs.writeFileSync('src/index.css', css);
